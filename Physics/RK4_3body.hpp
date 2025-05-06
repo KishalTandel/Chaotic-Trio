b6c34d5509872faf02d25d0d8b09b4e240b7c12a
Kishal Tandel <kishaltandel01@gmail.com>
2025-05-06 16:25:37 +0530

    Add files via upload

---
+++ Physics/RK4_3body.hpp
@@ -0,0 +1,46 @@
+//
+//  Created by Kishal Tandel on 4/5/2025.
+//
+
+#ifndef RK4_3body_hpp
+#define RK4_3body_hpp
+
+#include <stdio.h>
+#include <vector>
+#include "C:/Users/Admin/OneDrive/Desktop/Main/include/Eigen/Dense"
+
+
+using namespace Eigen ;
+
+// Project specific typing
+
+typedef Array<Vector2d,Dynamic,1> vecArray;
+typedef Array<vecArray, Dynamic, 1> vecArray2;
+
+class RK4_3body {
+    
+public:
+    
+    // Constructor
+    
+    RK4_3body(float m1,float m2,float m3){
+        Mass1 = m1;
+        Mass2 = m2;
+        Mass3 = m3;
+    };
+    
+    // Methods
+    
+    Vector2d bodyFunc(Vector2d r1,Vector2d r2,Vector2d r3,float mass1,float mass2,float mass3);
+    vecArray2 Run(float step, int N, Vector2d r1_0 , Vector2d r2_0 , Vector2d r3_0, const Vector2d v1_0 , Vector2d v2_0 , Vector2d v3_0);
+    
+private:
+    
+    float G = 1;
+    float Mass1;
+    float Mass2;
+    float Mass3;
+    
+    
+};
+#endif /* RK4_3body_hpp */
